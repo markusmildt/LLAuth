bffd0334c1a195d2a54062cf8756fc69da2cb79c
markusmildt <49172015+markusmildt@users.noreply.github.com>
2019-09-11 01:01:58 -0400

    Upload Main.java
    The main methods of the discord bot.  Currently with some unfinished and temp commands.

---
+++ Main.java
@@ -0,0 +1,70 @@
+package me.authbot.BoT;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import javax.security.auth.login.LoginException;
+import java.sql.*;
+
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken("NjE4OTY1NDI0MzI3MDk4Mzcw.XXBXsA.oC2L7WaEjMYOeoKXr7zoyeqbuvw"); //setting token
+        builder.addEventListeners(new Main());
+        builder.build();
+
+        sqlConnect(); //connecting to the SQLite database
+
+        //get auth code - auth
+    }
+
+    //runs whenever a message is received in chat
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        //prevents bots becoming stuck in infinite loops
+        if(event.getAuthor().isBot() || event.getAuthor().isFake()){
+            return;
+        }
+
+        //temp test command
+        if(event.getMessage().getContentRaw().equalsIgnoreCase("test")) {
+            String msg = event.getMessage().getContentRaw();
+            event.getChannel().sendMessage("We received the string " + msg + " from " + event.getAuthor().getName()).queue();
+        }
+
+        //request to verify command
+        if(event.getMessage().getContentRaw().equalsIgnoreCase("ll.verify")){
+            event.getChannel().sendMessage(event.getAuthor().getAsMention() + "\nHere is your verification code: ").queue();
+            event.getMessage().delete().queue(); //deletes the user's command
+        }
+
+        //help command
+        if(event.getMessage().getContentRaw().equalsIgnoreCase("ll.help")){
+            event.getChannel().sendMessage(event.getAuthor().getAsMention() + "\n**AuthBot Commands:** \n*ll.verify:* receive your verification code" +
+                    "\n*test:* a temporary test command").queue();
+        }
+    }
+
+
+    //handles connecting to the SQLite database
+    public static void sqlConnect(){
+        Connection conn = null;
+        try{
+            String url = "url"; //place SQLite url here
+
+            conn = DriverManager.getConnection(url);
+            System.out.println("Established connection to url " + url);
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        } finally{
+            try{
+                if(conn != null){
+                    conn.close();
+                }
+            } catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
