3577ab34b167cbc738ca6feadb4b5019f6196807
Markus <49172015+markusmildt@users.noreply.github.com>
2019-09-14 12:02:49 -0400

    SQLManager
    Pretty much finished the SQLManager class

--- SQLManager.java
+++ SQLManager.java
@@ -1,15 +1,25 @@
 package me.authbot.BoT;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLManager {
-    public static void connect(){
+
+    private String url = "url goes here";
+
+    /**
+     * SQLManager Constructor
+     */
+    public SQLManager(){
+        connect(); //connecting to url
+        createTable(); //creating table
+    }
+
+    /**
+     * Connect to the SQLite Database
+     */
+    private void connect(){
         Connection connection = null;
 
         try{
-            String url = "url"; //write SQLite URL here
-
             connection = DriverManager.getConnection(url);
 
             System.out.println("Connection established to " + url);
@@ -26,8 +36,45 @@ public class SQLManager {
         }
     }
 
-    public static void main(String[] args){
-        connect();
+    /**
+     * Creates a tabled called "players" with columns "IGN" and "Verified"
+     */
+    private void createTable(){
+
+        String sql = "CREATE TABLE IF NOT EXISTS players (\n"
+                + " ign text PRIMARY KEY, \n"
+                + " verified booelan NOT NULL\n"
+                +");";
+
+        try{
+            Connection connection = DriverManager.getConnection(url);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Inserts records into the table
+     * @param ign - Player's in game name
+     * @param verified - whether or not the player is verified
+     */
+    public void insert(String ign, boolean verified){
+
+        String sql = "INSERT INTO players(ign, verified) VALUES(?,?)";
+
+        try{
+            Connection connection = DriverManager.getConnection(url);
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, ign);
+            pstmt.setBoolean(2, verified);
+
+
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
 
